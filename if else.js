09586fc1cee94b8406704017a61a26e118b94e4a
Dhruvmodi418 <125945328+Dhruvmodi418@users.noreply.github.com>
2026-02-08 19:58:33 +0530

    Add files via upload

---
+++ if else.js
@@ -0,0 +1,25 @@
+let age=16;
+
+if(age>=18){
+    console.log("You are eligible for driving")
+}
+else {
+    console.log("You are not eligible for driving")
+}
+
+a=10
+b=10
+x=0
+
+if(a>b){
+    x= (a+b)
+}
+else if (a<b){
+    x=(b-a)
+}
+else{
+    x=(a*b)
+}
+
+
+console.log(x);
